20c3549ccc96ff24103d1c7f76c756cc2cf250a0
liuxianfeng521 <735801190@qq.com>
2018-09-14 10:57:12 +0800

    vue-element-admin 初始化

---
+++ src/views/ahahaha.vue
@@ -0,0 +1,16 @@
+<template>
+
+</template>
+
+<script>
+  export default {
+    name: "ahahaha.vue",
+    data() {
+      return {}
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/ccc.vue
@@ -0,0 +1,16 @@
+<template>
+
+</template>
+
+<script>
+  export default {
+    name: "ccc.vue",
+    data() {
+      return {}
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
